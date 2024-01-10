44db84529e3a9a47fff823cd895b4566a92436fd
Lokesh Krishna Cherukuri <lokeshkrishnacherukuri@Lokeshs-MacBook-Air.local>
2024-01-10 10:40:38 +0530

    modified userrouter.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const Playlist = require('../models/Playlist');
+const Playlist = require('../models/playlist');
 
 
 
