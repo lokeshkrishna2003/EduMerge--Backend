64113bbf4e2ece1a3fa43efd983fd41305d693b0
Lokesh Krishna Cherukuri <lokeshkrishnacherukuri@Lokeshs-MacBook-Air.local>
2024-01-12 00:52:48 +0530

    removed unwanted from the userroutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
-const Playlist = require("../models/playlist");
+
 
 const bcrypt = require("bcrypt");
 const { authenticateUser } = require("../middleware/auth"); // Ensure the correct path
